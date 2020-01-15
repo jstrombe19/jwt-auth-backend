6355d528f8ae4a8bed4dbe535e350cb3228e5387
Jared Stromberg <jaredstromberg@gmail.com>
2020-01-15 13:09:44 -0700

    Added authentication controller and login route

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,20 @@
+require 'jwt'
+
+class AuthenticationController < ApplicationController
+    def login
+        user = User.find_by(username: params[:username])
+
+        if (!user)
+            render status: :unauthorized
+        else
+            if user.authenticate(params[:password])
+                secret_key = Rails.application.secrets.secret_key_base[0]
+                token = JWT.encode(user, secret_key)
+
+                render json: {token: token} 
+            else
+                render status: :unauthorized
+            end
+        end 
+    end
+end
