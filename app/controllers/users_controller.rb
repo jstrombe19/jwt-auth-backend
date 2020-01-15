8a6621b610ef4e0ee8f7339b807c949ccd3eb6dc
Jared Stromberg <jaredstromberg@gmail.com>
2020-01-15 12:19:07 -0700

    Initial commit of JWT Rails backend. User has a name, username, email, and password - passwords are secure using bcrypt. Strong params are used in the Users controller.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+    def create
+        @user = User.create(user_params)
+        render json: @user 
+    end
+
+
+    private
+
+    def user_params 
+        params.require(:user).permit([:name, :username, :email, :password])
+    end
+
+end
